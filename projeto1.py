827deb0f8505d5913623e79e7f27d91813018ec7
donmarchiori <yuri.mendonca.marchiori@gmail.com>
2023-12-18 17:35:55 -0300

    second commit

--- projeto1.py
+++ projeto1.py
@@ -1,11 +1,11 @@
 print("Usando for:")
 for andar in range(20, 0, -1):
     if andar != 13:
-        print(andar)
+        print("Esse é o andar numero: ", andar)
 
 print("\nUsando while:")
 andar = 20
 while andar >= 1:
     if andar != 13:
-        print(andar)
+        print("Esse é o andar numero: ", andar)
     andar -= 1
